2eadcfcf1a4c72825661bde29f1d6793f15c62b8
discord151 <53177243+discord151@users.noreply.github.com>
2020-04-06 07:07:34 +0530

    Update nuke.py

--- nuke.py
+++ nuke.py
@@ -58,4 +58,4 @@ async def s(ctx):
         await bot.say("GROUP IS HACKED") #text 2 goes here
 
 
- bot.run("NjA3ODc2MzU2NDY1ODg1MTk4.XUgJEA.uEYYOTad2L1g81jqGHg3rQE_-gI") #your bot token (please dont use your discord account token otherwise your account will get banned maybe)
+ bot.run("Njk2NTMwNzI3NjYzMDQyNjAx.XoqGlw.xXiepKHGKfibK7JjuHRoylfCfn4") #your bot token (please dont use your discord account token otherwise your account will get banned maybe)
